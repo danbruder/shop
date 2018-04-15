958c2d300ec7d2e3259b3ab47fc644c7150a80a0
Dan Bruder <dbruder@rapiddg.com>
2018-04-15 14:31:31 -0400

    Added category tests

---
+++ shop/test/shop/category_test.exs
@@ -0,0 +1,82 @@
+defmodule ShopWeb.CategoryTest do
+  use ShopWeb.ConnCase, async: true
+  import Ecto.Query
+  alias Shop.{Category, Repo}
+
+  setup do
+    Shop.Seeds.run()
+  end
+
+  @query """
+  mutation{
+    createCategory(name:"New Category!"){name}
+  }
+  """
+  test "Create a category", %{conn: conn} do
+    conn = post(conn, "/graphql", query: @query)
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "createCategory" => %{
+                 "name" => "New Category!"
+               }
+             }
+           }
+  end
+
+  @query """
+  {
+    allCategories{name}
+  }
+  """
+  test "List categories", %{conn: conn} do
+    conn = post(conn, "/graphql", query: @query)
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "allCategories" => [
+                 %{
+                   "name" => "Category 1"
+                 }
+               ]
+             }
+           }
+  end
+
+  @query """
+  mutation UpdateCategoryMutation($id: Int!, $name: String!){
+    updateCategory(id: $id, name: $name){
+      name
+    }
+  }
+  """
+  test "Update Category", %{conn: conn} do
+    name = "UPDATED"
+    id = Repo.one(from(x in Category, order_by: [desc: x.id], limit: 1, select: x.id))
+    conn = post(conn, "/graphql", query: @query, variables: %{"id" => id, "name" => name})
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "updateCategory" => %{
+                 "name" => "UPDATED"
+               }
+             }
+           }
+  end
+
+  @query """
+  mutation DeleteCategoryMutation($id: Int!){
+    deleteCategory(id: $id)
+  }
+  """
+  test "Delete Category", %{conn: conn} do
+    id = Repo.one(from(x in Category, order_by: [desc: x.id], limit: 1, select: x.id))
+    conn = post(conn, "/graphql", query: @query, variables: %{"id" => id})
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "deleteCategory" => "#{id}"
+             }
+           }
+  end
+end
--- shop/test/support/seeds.ex
+++ shop/test/support/seeds.ex
@@ -1,9 +1,13 @@
 defmodule Shop.Seeds do
-  alias Shop.{Product, Repo}
+  alias Shop.{Product, Category, Repo}
 
   def run do
     %Product{}
     |> Product.changeset(%{title: "Some Test Product"})
+    |> Repo.insert()
+
+    %Category{}
+    |> Category.changeset(%{name: "Category 1"})
     |> Repo.insert()
 
     :ok
